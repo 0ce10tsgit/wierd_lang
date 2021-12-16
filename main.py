86e7f4c59272196704303b367d5fd8f486300163
Oyon Ganguli <96030922+0ce10tsgit@users.noreply.github.com>
2021-12-16 15:41:14 +0000

    Main with broken

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ jokes = ['Why was the cow afraid? He was a cow-herd.','What dog keeps the best t
 print("made by 0ce10t")
 print('github.com/0ce10tsgit/wierd_lang')
 print("type : '>help' for help")
-print("input command")      
+print("input command")
 def arr_eval(content):
   global arrays
   try:
@@ -64,6 +64,7 @@ def array_m(content):
   #array seal []
   spaced_content = content.split(" ")
   name = eval_var(spaced_content[1]) 
+    
   items = len(spaced_content)-2
   items_to_add = spaced_content[2:]
   num = 0
@@ -83,15 +84,13 @@ def output_method(tprint):
   if do_debug_out:
     print(tprint)
 def eval_var(name):
-  global variables
-  global arrays
-
+  global varaibles
   try:
     tor = int(name)
     return tor 
   except:
     if name[0] == '*':
-      return name[1:] 
+      return name[1:]
     if name[0] == '@':
       return len(eval_var(name[1:]))
     if name[0] == '#':
